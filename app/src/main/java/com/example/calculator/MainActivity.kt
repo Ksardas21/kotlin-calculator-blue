12223193678cd3f944099fdfeb3778865d0d8dd8
Ksardas21 <Ksardas21@github.com>
2020-09-08 01:49:11 +0400

    kotlin Calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -16,100 +16,110 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var dot = false
+        var dot = true
         var operation = false
+        var dotOp = false
         var negative = true
 
         buttonDivide.setOnClickListener {
-            if(!operation && currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*', '.'))setTextFields("")
-            if(currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*')) currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "/"
-            if(operation && currentOpperand.text.toString().lastOrNull() !in arrayOf('+', '-', '/', '*','.') && currentOpperand.text.isNotEmpty())setTextFields("/")
+
+            if(!operation)setTextFields("")
+            if(currentOpperand.text.lastOrNull() in arrayOf('-', '+', '/', '*'))
+            currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "/"
+            else setTextFields("/")
+            if(!dot) {dotOp = true}
+            if(dot) {dotOp = false}
             dot = true
             operation = false
         }
         buttonMultiply.setOnClickListener {
-            if(!operation && currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*', '.'))setTextFields("")
-            if(currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*'))currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "*"
-            if(operation && currentOpperand.text.toString().lastOrNull() !in arrayOf('+', '-', '/', '*','.') && currentOpperand.text.isNotEmpty()) setTextFields("*")
+            if(!operation)setTextFields("")
+            if(currentOpperand.text.lastOrNull() in arrayOf('-', '+', '/', '*'))
+            currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "*"
+            else setTextFields("*")
+            if(!dot) {dotOp = true}
+            if(dot) {dotOp = false}
             dot = true
             operation = false
         }
         buttonMinus.setOnClickListener {
-            if(!operation && currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*', '.'))setTextFields("")
-            if(currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*')) currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "-"
-            if(operation && currentOpperand.text.toString().lastOrNull() !in arrayOf('+', '-', '/', '*','.') && currentOpperand.text.isNotEmpty()) setTextFields("-")
+
+            if(!operation)setTextFields("")
+            if(currentOpperand.text.lastOrNull() in arrayOf('-', '+', '/', '*'))
+                currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "-"
+            else setTextFields("-")
+            if(!dot) {dotOp = true}
+            if(dot) {dotOp = false}
             dot = true
             operation = false
         }
         buttonPlus.setOnClickListener {
-            if(!operation && currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*', '.'))setTextFields("")
-            if(currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*')) currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "+"
-            if(operation && currentOpperand.text.toString().lastOrNull() !in arrayOf('+', '-', '/', '*','.') && currentOpperand.text.isNotEmpty()) { setTextFields("+")}
+
+            if(!operation)setTextFields("")
+            if(currentOpperand.text.lastOrNull() in arrayOf('-', '+', '/', '*'))
+            currentOpperand.text = currentOpperand.text.toString().dropLast(1) + "+"
+            else setTextFields("+")
+            if(!dot) {dotOp = true}
+            if(dot) {dotOp = false}
             dot = true
             operation = false
+            negative = true
         }
-        buttonPlusMinus.setOnClickListener {
-            if((currentOpperand.text.firstOrNull() in arrayOf('+', '-', '/', '*'))&& !negative){
-                setTextFields("-")
-
-            }
-            if(currentOpperand.text.endsWith("-") || negative) {
-                currentOpperand.text.toString().dropLast(1)
-                negative = false
-            }
-        }
-
-
 
         buttonOne.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "1"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "1"
             else setTextFields("1")
             operation = true
         }
         buttonTwo.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "2"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "2"
             else setTextFields("2")
             operation = true
         }
         buttonThree.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "3"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "3"
             else setTextFields("3")
             operation = true
         }
         buttonFour.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "4"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "4"
             else setTextFields("4")
             operation = true
         }
         buttonFive.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "5"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "5"
             else setTextFields("5")
             operation = true
         }
         buttonSix.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "6"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "6"
             else setTextFields("6")
             operation = true
         }
-
         buttonSeven.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "7"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "7"
             else setTextFields("7")
             operation = true
         }
-
         buttonEight.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "8"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "8"
             else setTextFields("8")
             operation = true
         }
-
         buttonNine.setOnClickListener {
-            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text = "" + "9"
+            if (currentOpperand.text.toString().startsWith("0") && currentOpperand.text.toString().length < 2) currentOpperand.text =
+                "" + "9"
             else setTextFields("9")
             operation = true
         }
-
         buttonZero.setOnClickListener {
             when {
                 currentOpperand.text.toString().length == 1 && currentOpperand.text.toString().startsWith("0") -> setTextFields("")
@@ -117,52 +127,40 @@ class MainActivity : AppCompatActivity() {
             }
             operation = true
         }
-
         buttonDot.setOnClickListener {
-            when {
-                currentOpperand.text.toString().isEmpty() || currentOpperand.text.toString().last() in arrayOf('+', '-', '/', '*', '(', ')')-> setTextFields("")
-                currentOpperand.text.toString().indexOf('.')  < 1 && !dot -> setTextFields(".")
-                !dot -> setTextFields("")
-                dot && currentOpperand.text.toString().lastOrNull() !in arrayOf('+', '-', '/', '*', '.', ')', '(')-> setTextFields(".")
-                else -> setTextFields("")
-            }
+            if(!dot) {setTextFields("")}
+            if(currentOpperand.text.endsWith(".")) {setTextFields("")}
+            else if(dot){setTextFields(".")}
             dot = false
-            operation = false
+            operation = true
         }
-
         buttonClearAll.setOnClickListener {
             currentOpperand.text = ""
             Result.text = ""
+            dot = true
+            operation = false
+            negative = true
         }
-
         buttonClear.setOnClickListener {
-            if (currentOpperand.text.toString().endsWith('.')) currentOpperand.text = currentOpperand.text.toString().dropLast(0)
-                dot = true
+            if (currentOpperand.text.lastOrNull() in arrayOf('+', '/', '-', '*') && dotOp){
                 operation = true
-            if (currentOpperand.text.toString().isNotEmpty())  currentOpperand.text = currentOpperand.text.toString().dropLast(1)
-                dot = false
+                dot = false}
+            if (currentOpperand.text.lastOrNull() in arrayOf('+', '/', '-', '*') && !dotOp){
                 operation = true
-            if (currentOpperand.text.toString().lastOrNull() in arrayOf('+', '-', '/', '*', '.')){
-                currentOpperand.text = currentOpperand.text.toString().dropLast(0)
-                dot = false
-                operation = true
-            }
-            operation = true
+                dot = true}
+            if (currentOpperand.text.endsWith(".")){ dot = true}
+            if (currentOpperand.text.toString().isNotEmpty()){ currentOpperand.text =
+                currentOpperand.text.toString().dropLast(1)}
         }
-
         buttonEquals.setOnClickListener {
             try {
-                val calculation =
-                    ExpressionBuilder(currentOpperand.text.toString()).build().evaluate()
-                val longAnswer = calculation.toLong()
-                if (currentOpperand.text.contains("."))
-                    Result.text = calculation.toString()
-                else Result.text = longAnswer.toString()
+                val calculation = ExpressionBuilder(currentOpperand.text.toString()).build().evaluate()
+                val answer = calculation.toDouble()
+                Result.text = answer.toString()
             }
             catch (e:Exception){
                 Log.d("Error", "Message: ${e.message}")
             }
-
         }
     }
     private fun setTextFields(str: String){
